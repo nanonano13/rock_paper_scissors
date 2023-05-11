ca652cada1eaea96260531e040e859cd5f037e20
Brandon Silva-Nano <brandon.silvanano@yahoo.com>
2023-05-11 15:11:30 -0500

    Generated random number

---
+++ script.js
@@ -0,0 +1,10 @@
+// Generate random number
+// Assign number to move
+// Return computer's result
+// 
+//
+function computerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    return number;
+}
+console.log(computerChoice())
