b6c7fa80ca936b0a3f3fe37789ca7f8b131524dc
Brandon Silva-Nano <brandon.silvanano@yahoo.com>
2023-07-26 20:02:53 -0500

    completed RPS

--- script.js
+++ script.js
@@ -3,8 +3,46 @@
 // Return computer's result
 // 
 //
+const choices = ["rock", "paper", "scissors"];
+//  Generate random number 1-3 
+
 function computerChoice() {
-    let number = Math.floor(Math.random() * 3);
-    return number;
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
 }
-console.log(computerChoice())
+// Determines winner of the round or if it is a tie
+
+function determineWinner(computerChoice, userChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie!"
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissor" && computerChoice === "paper")
+    ) {
+        return "You win!";
+    } else {
+        return "Computer wins!";
+    }
+}
+// Creates a function that grabs the results
+function displayResult(result) {
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = result;
+}
+// plays a game round
+function playGame(userChoice) {
+    const computer = computerChoice();
+    const result = determineWinner(userChoice, computer);
+    displayResult(result);
+}
+
+const rockBtn = document.getElementById("rock");
+rockBtn.addEventListener("click", () => playGame("rock"));
+
+const paperBtn = document.getElementById("paper");
+paperBtn.addEventListener("click", () => playGame("paper"));
+
+const scissorsBtn = document.getElementById("scissors");
+scissorsBtn.addEventListener("click", () => playGame("scissors"));
+
